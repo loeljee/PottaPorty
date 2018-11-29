2f06671e8974cf63cd743d53bb0d598b90024448
sergeymaguire <sergeymaguire@gmail.com>
2018-11-29 14:21:54 -0800

    updates

--- public/js/index.js
+++ public/js/index.js
@@ -79,10 +79,10 @@ function addMarkers(map, base, baseImage, restrooms) {
     };
     var goldStar = {
       path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
-      fillColor: 'yellow',
+      fillColor: 'red',
       fillOpacity: 0.8,
-      scale: .2,
-      strokeColor: 'gold',
+      scale: .009,
+      strokeColor: 'red',
       strokeWeight: 14
     };
     var marker = new google.maps.Marker({
