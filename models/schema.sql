d992383f60e8fd83804aba4cf875001e9e38f44f
Josh Rivenburgh <41762941+Jrivenburgh@users.noreply.github.com>
2018-12-01 12:25:03 -0800

    Update schema.sql (#17)

--- models/schema.sql
+++ models/schema.sql
@@ -7,8 +7,17 @@ CREATE TABLE bathrooms
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
     location varchar(255) NOT NULL,
+    keyRequired BOOLEAN DEFAULT false,
+    doorCode INTEGER(11) NULL,
+    cleanliness INTEGER(10) NULL,
+    seperatedByGender BOOLEAN DEFAULT true,
+    changingTable BOOLEAN DEFAULT false,
+    privacy INTEGER(10) NULL,
+    tpSoftness INTEGER(10) NULL,
     createdAt timestamp,
     updatedAt datetime,
 	PRIMARY KEY (id)
 );
-INSERT INTO `bathroom`.`bathrooms` (`name`, `location`) VALUES ('n1', 'l1');
+
+INSERT INTO bathroom.bathrooms (name, location, keyRequired, doorCode, cleanliness, seperatedByGender, changingTable, privacy, tpSoftness) 
+VALUES ('UCSD 1st floor Bathroom', '6256 Greenwich Dr San Diego, CA 92122', false, NULL, 9, TRUE, false, 10, 8);
