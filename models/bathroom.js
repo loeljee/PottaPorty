42cad43008166745199aa81d6139ed6ea6171d56
Josh Rivenburgh <41762941+Jrivenburgh@users.noreply.github.com>
2018-12-01 12:52:46 -0800

    Update bathroom.js (#18)

--- models/bathroom.js
+++ models/bathroom.js
@@ -1,7 +1,14 @@
 module.exports = function(sequelize, DataTypes) {
   var Bathroom = sequelize.define("Bathroom", {
     name: DataTypes.STRING,
-    location: DataTypes.STRING
+    location: DataTypes.STRING,
+    keyRequired: DataTypes.BOOLEAN,
+    doorCode: DataTypes.INTEGER,
+    cleanliness: DataTypes.INTEGER,
+    seperatedByGender: DataTypes.BOOLEAN,
+    changingTable: DataTypes.BOOLEAN,
+    privacy: DataTypes.INTEGER,
+    tpSoftness: DataTypes.INTEGER
   });
   return Bathroom;
 };
